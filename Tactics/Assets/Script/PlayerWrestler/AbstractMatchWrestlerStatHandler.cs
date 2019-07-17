d143c4b4aefb980bbe8b5211d9180d046bdca677
Yusufucote <Yusufucote@gmail.com>
2019-07-16 19:22:27 -0500

    removed unneeded scripts

--- Tactics/Assets/Script/PlayerWrestler/AbstractMatchWrestlerStatHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class AbstractMatchWrestlerStatHandler<T> {
-
-	public abstract void Initialize(T baseStatValue);
-}
